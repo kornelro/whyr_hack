5719d0b4647e2d0b8cda851ad9866baa4c0ae5b2
Kornel Romański <kornel.ro@gmail.com>
2020-09-24 09:09:24 +0200

    graph

--- challenge2/graph/model/comment.py
+++ challenge2/graph/model/comment.py
@@ -1,19 +1,31 @@
-from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Any
 
 from .user import User
 
 
-@dataclass
 class Comment:
-    by_id: int
-    by: User
-    id: int
-    parent_id: int
-    parent: Any
-    kids_ids: List[int]
-    kids: List[Any]
-    text: str
-    time: datetime
-    type: str
+
+    def __init__(
+        self,
+        by_id: int,
+        by: User,
+        id: int,
+        parent_id: int,
+        parent: Any,
+        kids_ids: List[int],
+        kids: List[Any],
+        text: str,
+        time: datetime,
+        type: str
+    ):
+        self.by_id = by_id
+        self.by = by
+        self.id = id
+        self.parent_id = parent_id
+        self.parent = parent
+        self.kids_ids = kids_ids
+        self.kids = kids
+        self.text = text
+        self.time = time
+        self.type = type
--- challenge2/graph/model/user.py
+++ challenge2/graph/model/user.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
 from typing import List, Any
 # from .comment import Comment
 
 
-@dataclass
 class User:
-    name: str
-    comments_ids: List[int]
-    # comments: List[Comment]
-    comments: List[Any]
+
+    def __init__(
+        self,
+        name: str,
+        comments_ids: List[int],
+        # comments: List[Comment]
+        comments: List[Any]
+    ):
+        self.name = name
+        self.comments_ids = comments_ids
+        self.comments = comments
